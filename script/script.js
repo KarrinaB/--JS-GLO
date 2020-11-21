6ee47e150305a52ecf685ecbc52135c6522e2b52
KarrinaB <carinabogomolowa@yandex.ru>
2020-11-21 14:11:05 +0300

    Создала и вывела в консоль переменные по ТЗ второго урока

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
-let money = 8;
-let income = 5;
-let addExpenses = 71;
-let deposit = 3;
-let mission  = 4;
-const period = 32;
+let money = 25000;
+let income = 'Подработка';
+let addExpenses = 'Коммунальные услуги, танцы, курсы';
+let deposit = true;
+let mission  = 150000;
+const period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
 
 alert('Hello, world!');
 

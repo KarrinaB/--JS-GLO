a5864642da3074cf42ab6d2a99b2e4a49eca625e
KarrinaB <carinabogomolowa@yandex.ru>
2020-11-23 00:36:06 +0300

    Fix(script/script.js): edits of style

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
-const money = 25000,income = 'Подработка',addExpenses = 'Коммунальные услуги, танцы, курсы',deposit = true,
-mission  = 150000,period = 6;let budgetDay = money/30;
-
-console.log(typeof money);console.log(typeof income);console.log(typeof deposit);console.log(addExpenses.length);console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
-console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');console.log(addExpenses.toLowerCase().split(', '));console.log(budgetDay);
+const money = 25000,
+  income = 'Подработка',
+  addExpenses = 'Коммунальные услуги, танцы, курсы',
+  deposit = true,
+  mission  = 150000,
+  period = 6,
+  budgetDay = money/30;
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
 console.log('Hello!');
 alert('Hello, world!');
 

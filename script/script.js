f844d482f6443fb6c1b15e3461315cc4a6ca6f28
KarrinaB <carinabogomolowa@yandex.ru>
2020-11-22 15:06:45 +0300

    Fix(script/script.js):edits in the code

--- script/script.js
+++ script/script.js
@@ -1,22 +1,9 @@
-let money = 25000;
-let income = 'Подработка';
-let addExpenses = 'Коммунальные услуги, танцы, курсы';
-let deposit = true;
-let mission  = 150000;
-const period = 6;
+const money = 25000,income = 'Подработка',addExpenses = 'Коммунальные услуги, танцы, курсы',deposit = true,
+mission  = 150000,period = 6;let budgetDay = money/30;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
-console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-
-let budgetDay = money/30;
-console.log(budgetDay);
-
+console.log(typeof money);console.log(typeof income);console.log(typeof deposit);console.log(addExpenses.length);console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');console.log(addExpenses.toLowerCase().split(', '));console.log(budgetDay);
+console.log('Hello!');
 alert('Hello, world!');
 
-console.log('Hello!'); 
+ 

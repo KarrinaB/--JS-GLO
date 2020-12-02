b273406a2e382a788e9fbf2edb3b774980a64b4b
KarrinaB <carinabogomolowa@yandex.ru>
2020-12-02 21:46:50 +0300

    Fixeeed.

--- script/script.js
+++ script/script.js
@@ -20,9 +20,28 @@ let appData = {
   budget: 'money',
   budgetDay: 0,
   budgetMonth: 0,
-  expensesMonth: 0,
-  
-
+  expensesMonth: 0 ,
+  targetMonth: 0,
+  asking: function() {
+    let oneQuest,
+      twoQuest;
+      for (let i = 0; i < 2; i++) {
+         oneQuest = prompt('Введите обязательную статью расходов');
+      do {
+        twoQuest = prompt('Во сколько это обойдется?');
+        } while (isNaN(parseFloat(twoQuest)));
+        appData.expenses[oneQuest] = Number(twoQuest);
+  };
+},
+  getExpensesMonth: function(){
+      for(let key in appData.expenses) {
+        appData.expensesMonth += appData.expenses[key];
+    };
+  },
+  getBudget: function() {
+    appData.budgetMonth = Number(appData.budget) - appData.expensesMonth;
+    appData.budgetDay = appData.budgetMonth / 30;
+  },
   getTargetMonth: function(){
     return appData.mission / appData.budgetMonth;
   },
@@ -40,57 +59,21 @@ let appData = {
       console.log('Что-то пошло не так');
     };
   },
-  asking: function() {
-    let oneQuest,
-      twoQuest;
-      for (let i = 0; i < 2; i++) {
-         oneQuest = prompt('Введите обязательную статью расходов');
-      do {
-        twoQuest = prompt('Во сколько это обойдется?');
-        } while (isNaN(parseFloat(twoQuest)));
-        appData.expenses[oneQuest] = Number(twoQuest);
-  };
-},
-  getExpensesMonth: function(){
-    let rez = 0;
-  for(let key in appData.expenses) {
-    rez += appData.expenses[key];
-    appData.expensesMonth = rez;
-  };
-  return rez;
-},
-
-  getBudget: function(){
-  appData.budgetMonth = Number(appData.budget) - appData.expensesMonth,
-  appData.budgetDay = appData.budgetMonth / 30;
-},
+  getThere: function (){
+    if (appData.getTargetMonth >= 0) {
+      console.log('Цель будет достигнута через:' + ' ' + Math.ceil (appData.getTargetMonth) + ' ' + 'месяцев');   
+    } else if (appData.getTargetMonth < 0) {
+      console.log('Цель не будет достигнута:(');
+    };
+  }
 };
+
 appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
+appData.getTargetMonth();
+appData.getStatusIncome();
+appData.getThere();
 
-//const income = 'Подработка',
-  //addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-  
-    
-//const expensesAmount = appData.getExpensesMonth();
-//const accumulatedMonth = appData.getAccumulatedMonth();
-
-
- 
-
-
-const targetMonth = appData.getTargetMonth();
-
-
-const getThere = function (){
-  if (targetMonth >= 0) {
-    console.log('Цель будет достигнута через:' + ' ' + Math.ceil (appData.getTargetMonth()) + ' ' + 'месяцев');   
-  } else if (targetMonth < 0) {
-    console.log('Цель не будет достигнута:(');
-};
-};
-getThere();
 
 console.log('Расходы за месяц: ' + appData.expensesMonth);
-console.log('Цель будет достигнута за: ' + )
-appData.getStatusIncome();
-appData.getTargetMonth();

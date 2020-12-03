8ef5edd25f75062533fb11356272efea07c1b89e
KarrinaB <carinabogomolowa@yandex.ru>
2020-12-03 17:38:58 +0300

    Feat(script/script.js): feat code & fixed style.

--- script/script.js
+++ script/script.js
@@ -20,19 +20,37 @@ let appData = {
   budget: money,
   budgetDay: 0,
   budgetMonth: 0,
-  expensesMonth: 0 ,
+  expensesMonth: 0,
   targetMonth: 0,
+  percentDeposit: 0,
+  moneyDeposit: 0,
   asking: function() {
+    if(confirm('Есть ли у Вас дополнительный заработок?')) {
+      let itemIncome, cashIncome;
+      do {
+        itemIncome = prompt('Какой у Вас дополнительный заработок?');
+      } while (!isNaN(itemIncome));
+
+      do {
+        cashIncome = prompt('Сколько Вы зарабатываете на этом в месяц?');
+      } while (isNaN(parseFloat(cashIncome)));
+      appData.income[itemIncome] = cashIncome;
+    };
+    
     let oneQuest,
       twoQuest;
       for (let i = 0; i < 2; i++) {
-         oneQuest = prompt('Введите обязательную статью расходов');
-      do {
+        do { 
+          oneQuest = prompt('Введите обязательную статью расходов');
+        appData.addExpenses += oneQuest.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ') + ', ';
+        } while (!isNaN(oneQuest));
+        do {
         twoQuest = prompt('Во сколько это обойдется?');
         } while (isNaN(parseFloat(twoQuest)));
         appData.expenses[oneQuest] = Number(twoQuest);
   };
-},
+        appData.deposit = confirm('Есть ли у Вас депозит в банке?');
+  },
   getExpensesMonth: function(){
       for(let key in appData.expenses) {
         appData.expensesMonth += appData.expenses[key];
@@ -59,12 +77,27 @@ let appData = {
       console.log('Что-то пошло не так');
     };
   },
-  getThere: function (){
+  getThere: function () {
     if (appData.targetMonth >= 0) {
       console.log('Цель будет достигнута через:' + ' ' + Math.ceil (appData.targetMonth) + ' ' + 'месяцев');   
     } else if (appData.targetMonth < 0) {
       console.log('Цель не будет достигнута:(');
     };
+  },
+  getInfoDeposit: function () {
+    if(appData.deposit = true){
+      do {
+        appData.percentDeposit = prompt('Какой годовой процент?');
+      } while (isNaN(parseFloat(appData.percentDeposit)));
+      do {
+        appData.moneyDeposit = prompt('Какая сумма заложена?');
+      } while (isNaN(parseFloat(appData.moneyDeposit)));
+      
+    }
+  },
+  calcSavedMoney: function() {
+    return appData.budgetMonth * appData.period;
+
   }
 };
 
@@ -74,9 +107,9 @@ appData.getBudget();
 appData.getTargetMonth();
 appData.getStatusIncome();
 appData.getThere();
-
-
-console.log('Расходы за месяц: ' + appData.expensesMonth);
+appData.getInfoDeposit();
+console.log(appData.addExpenses);
+console.log('Расходы за месяц: ' + appData.expensesMonth );
 
 console.log('Наша программа включает в себя данные: ');
   for(let key in appData){
